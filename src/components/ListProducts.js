a4b67a1d47cf61907feafd2e661e0157e39b6ccc
Ognjen <ognjenmili1993@gmail.com>
2018-03-10 18:04:42 +0100

    TopProdusts

--- src/components/ListProducts.js
+++ src/components/ListProducts.js
@@ -1,32 +1,23 @@
 import React from 'react';
 import 'css/TopProducts.css';
 import Product from 'components/product';
-import picture1 from 'assets/imgTopProducts/picture1.jpg';
-import picture2 from 'assets/imgTopProducts/picture2.jpg';
-import picture3 from 'assets/imgTopProducts/picture3.jpg';
 
 let ListProducts = [
-  <Product
-    key="first"
-    src={picture1}
-    name="Sedište malo A3/APN6"
-    category="MOPEDI"
-    prise="1600 din"
-  />,
-  <Product
-    key="second"
-    src={picture2}
-    name="Sedište malo A3/APN6"
-    category="MOPEDI"
-    prise="1600 din"
-  />,
-  <Product
-    key="third"
-    src={picture3}
-    name="Sedište malo A3/APN6"
-    category="MOPEDI"
-    prise="1600 din"
-  />
+  {
+    name: 'Sedište malo A3/APN6',
+    category: 'MOPEDI',
+    prise: '1600 din'
+  },
+  {
+    name: 'Sedište malo A3/APN6',
+    category: 'MOPEDI',
+    prise: '1600 din'
+  },
+  {
+    name: 'Sedište malo A3/APN6',
+    category: 'MOPEDI',
+    prise: '1600 din'
+  }
 ];
 
 export default ListProducts;
--- src/components/TopProducts.js
+++ src/components/TopProducts.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
 import 'css/TopProducts.css';
-import Responsive from 'components/Responsive';
 import src from 'assets/imgTopProducts/svi-proizvodi-btn-icon.png';
+import src1 from 'assets/imgTopProducts/brendovi-arrow-left.png';
+import src2 from 'assets/imgTopProducts/brendovi-arrow-right.png';
+import ListProducts from 'components/ListProducts';
+import Product from 'components/product';
+import picture1 from 'assets/imgTopProducts/picture1.jpg';
+import picture2 from 'assets/imgTopProducts/picture2.jpg';
+import picture3 from 'assets/imgTopProducts/picture3.jpg';
 
 export default class TopProducts extends Component {
   constructor(props) {
@@ -18,11 +24,47 @@ export default class TopProducts extends Component {
       <div className="cointainer">
         <p className="title">Istaknuti Proizvodi</p>
         <div className="line" />
-        <Responsive
-          element={element}
-          leftClick={leftClick}
-          rightClick={rightClick}
-        />
+        <div className="arrows">
+          <img
+            onClick={() => leftClick()}
+            src={src1}
+            alt="Arrow left"
+            className="leftarrow"
+          />
+          <img
+            onClick={() => rightClick()}
+            src={src2}
+            alt="Arrow right"
+            className="rightarrow"
+          />
+        </div>
+        <div className="products">
+          <div className="show" id="firstProduct">
+            <Product
+              src={picture1}
+              name={ListProducts[0].name}
+              category={ListProducts[0].category}
+              prise={ListProducts[0].prise}
+            />
+          </div>
+          <div className="hide" id="secondProduct">
+            <Product
+              src={picture2}
+              name={ListProducts[1].name}
+              category={ListProducts[1].category}
+              prise={ListProducts[1].prise}
+            />
+          </div>
+          <div className="hide" id="thirdProduct">
+            <Product
+              src={picture3}
+              name={ListProducts[2].name}
+              category={ListProducts[2].category}
+              prise={ListProducts[2].prise}
+            />
+          </div>
+        </div>
+
         <div className="button">
           <img className="arrow" src={src} alt="Icon arrow" />
           POGLEDAJ SVE PROIZVODE
@@ -30,16 +72,38 @@ export default class TopProducts extends Component {
       </div>
     );
   }
-  leftClick = () => {
-    const { state: { element } } = this;
-    let t = (element + 1) % 3;
-    this.setState({ element: t });
-  };
-
   rightClick = () => {
-    const { state: { element } } = this;
-    let t;
-    element === 0 ? (t = 2) : (t = element - 1);
-    this.setState({ element: t });
+    var firstProduct = document.getElementById('firstProduct');
+    var secondProduct = document.getElementById('secondProduct');
+    var thirdProduct = document.getElementById('thirdProduct');
+    if (firstProduct.className == 'show') {
+      firstProduct.className = 'hide';
+      secondProduct.className = 'show';
+    } else {
+      if (secondProduct.className == 'show') {
+        secondProduct.className = 'hide';
+        thirdProduct.className = 'show';
+      } else {
+        thirdProduct.className = 'hide';
+        firstProduct.className = 'show';
+      }
+    }
+  };
+  leftClick = () => {
+    var firstProduct = document.getElementById('firstProduct');
+    var secondProduct = document.getElementById('secondProduct');
+    var thirdProduct = document.getElementById('thirdProduct');
+    if (firstProduct.className == 'show') {
+      firstProduct.className = 'hide';
+      thirdProduct.className = 'show';
+    } else {
+      if (secondProduct.className == 'show') {
+        secondProduct.className = 'hide';
+        firstProduct.className = 'show';
+      } else {
+        thirdProduct.className = 'hide';
+        secondProduct.className = 'show';
+      }
+    }
   };
 }
