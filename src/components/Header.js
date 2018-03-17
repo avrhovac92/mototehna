1a5ae117ac9b85c5e3f6ba5b33028646a7b80f63
milicav24 <36275087+milicav24@users.noreply.github.com>
2018-03-17 13:08:42 +0100

    Izmene

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,43 @@
 import React, { Component } from 'react';
+import { Navbar, Nav, NavItem, NavDropdown } from 'react-bootstrap';
 import 'css/Header.css';
-import Menu from 'components/Menu.js';
+import logo from 'assets/slike/logo.png';
+import hamburger from 'assets/slike/hamurger-menu.png';
 
 class Header extends Component {
   render() {
     return (
-      <div>
-        <Menu />
+      <div className="container">
+        <img alt="logo" src={logo} className="logo" />
+        <Navbar collapseOnSelect className="naslov">
+          <Navbar.Header>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav>
+              <NavItem className="text" eventKey={1}>
+                Naslovna
+              </NavItem>
+              <NavItem divider />
+              <NavItem className="text" eventKey={2}>
+                Prodavnica
+              </NavItem>
+              <NavItem divider />
+              <NavItem className="text" eventKey={3}>
+                Usluge
+              </NavItem>
+              <NavItem divider />
+              <NavItem className="text" eventKey={4}>
+                O Nama
+              </NavItem>
+              <NavItem divider />
+              <NavItem className="text" eventKey={5}>
+                Kontakt
+              </NavItem>
+              <NavItem divider />
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'css/index.css';
 import App from './App';
-import Header from 'components/Header';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
