77b45bd5511b7c5a82859d569d853cbf4b1496aa
MiroslavRadulovic <miroslav.radulovic92@gmail.com>
2018-03-20 12:56:48 +0100

    message

--- src/screens/Register.js
+++ src/screens/Register.js
@@ -1,9 +1,10 @@
-import React, { Component } from 'react';
-import 'css/App.css';
-import Header from 'components/Header';
+import React, { Component } from "react";
+import "css/App.css";
+import Header from "components/Header";
 
-import Footer from 'components/Footer';
-import AboutUs from 'components/AboutUs';
+import Footer from "components/Footer";
+import AboutUs from "components/AboutUs";
+import CustomerInformations from "components/CustomerInformations";
 
 class Register extends Component {
   render() {
@@ -11,6 +12,7 @@ class Register extends Component {
       <div>
         <Header />
         <AboutUs />
+        <CustomerInformations />
         <Footer />
       </div>
     );
